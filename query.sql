75eb21b6d86bc7110d8f41c0b9fc47bc09963af9
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2023-02-17 11:18:01 +0700

    eda 1

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
 /* Get the number of unique users, number of orders, and total sale price per status and month */
-SELECT DATE_TRUNC(DATE(created_at) ) as order_time,
+SELECT DATE_TRUNC(DATE(created_at) ) as month_time,
        status,
        COUNT(DISTINCT user_id) as total_unique_users,
        COUNT(DISTINCT order_id) as total_orders,
@@ -8,3 +8,5 @@ FROM `bigquery-public-data.thelook_ecommerce.order_items`
 WHERE created_at between '2019-01-01' and '2022-09-01'
 GROUP BY 1, 2
 ORDER BY 2, 1
+
+/* get the 
